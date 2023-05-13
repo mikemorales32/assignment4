5a0183c1db1df5d773c9553e02422a771680ef02
mikemorales32 <michael.morales@unomaha.edu>
2023-05-13 16:51:30 -0500

    update

---
+++ vue_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/michaelmorales/PycharmProjects/movies/assignment4/vue_env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
